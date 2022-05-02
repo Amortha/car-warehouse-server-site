a10bcdcbe41b5025767e9cdbfa5457d0bef551dd
Amortha <amitsharma1998k@gmail.com>
2022-05-03 00:28:41 +0600

    commit

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 require('dotenv').config();
 
+ //user dbuser1
+ // paswoard 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4bshi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
